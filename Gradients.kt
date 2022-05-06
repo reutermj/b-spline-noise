5fbb633ba4298e9b4e2cba6407774b15c1ba3420
mark <github@markreuter.dev>
2022-05-06 11:24:39 -0500

    Initial Commit

--- Gradients.kt
+++ Gradients.kt
@@ -1,3 +1,8 @@
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.random.Random
+
 sealed class Vector(val size: Int)
 data class Vector1(val x: Float): Vector(1) {
     fun dot(x1: Float): Float {
@@ -25,3 +30,26 @@ data class Gradients<V: Vector>(val grads: Array<V>) {
         return grads[index.rem(grads.size)]
     }
 }
+
+fun genPerlin1Grads(i: Int, rand: Random): Gradients<Vector1> {
+    val arr = Array(i) {
+        Vector1(rand.nextFloat() * 2f - 1f)
+    }
+    return Gradients(arr)
+}
+
+fun genPerlin2Grads(i: Int, rand: Random): Gradients<Vector2> {
+    val arr = Array(i) {
+        val rad = rand.nextFloat() * 2 * PI
+        Vector2(cos(rad).toFloat(), sin(rad).toFloat())
+    }
+    return Gradients(arr)
+}
+
+val defaultPerlin3Grads: Gradients<Vector3> =
+    Gradients(
+        arrayOf(
+            Vector3(1f, 1f, 0f), Vector3(-1f, 1f, 0f), Vector3(1f, -1f, 0f), Vector3(-1f, -1f, 0f),
+            Vector3(1f, 0f, 1f), Vector3(-1f, 0f, 1f), Vector3(1f, 0f, -1f), Vector3(-1f, 0f, -1f),
+            Vector3(0f, 1f, 1f), Vector3(0f, -1f, 1f), Vector3(0f, 1f, -1f), Vector3(0f, -1f, -1f))
+    )
